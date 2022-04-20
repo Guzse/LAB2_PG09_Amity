03c2153736a33110a3591952dad469760375f159
SeppeBe1 <seppe.bellens@hotmail.nl>
2022-04-20 16:10:56 +0200

    fix merge conflict login.js

--- src/Pages/login/login.js
+++ src/Pages/login/login.js
@@ -5,33 +5,6 @@ import Container from "@mui/material/Container";
 import Navigation from "../../Components/Navigation/Navigation";
 import logo from '../../assets/images/SVG/Logo_A.svg';
 
-<<<<<<< HEAD
-function Login() {
-return(
-  // JSX code for login form
-    <div className="login">
-      <div className="logoDiv"> 
-        <img className="logoMain" src={require('../../images/2x/Woordmerk_A@2x.png')} alt="Logo"/>
-      </div>
-      <form>
-          <h2 className="titleForm">Login</h2>
-        <div className="username-container">
-          <label>Username </label>
-          <input type="text" name="username" required />
-        </div>
-        <div className="password-container">
-          <label>Password </label>
-          <input type="password" name="pass" required />
-
-        </div>
-        <div className="button-container">
-            <Link className="btnLogin" to="/mainscreen">Login</Link>
-            <Link className="btnLogin" to="/registration">Create account</Link>
-        </div>
-      </form>
-    </div>
-  );
-=======
 const handleSubmit = (event) => {
     event.preventDefault();
 };
@@ -69,7 +42,6 @@ function Login() {
             </Container>
         </>
     );
->>>>>>> 87591165850eb5188d82edf727516e5c94547047
 }
 
 export default Login;
