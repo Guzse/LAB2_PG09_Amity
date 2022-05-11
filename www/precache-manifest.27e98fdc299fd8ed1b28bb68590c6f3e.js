36ab2d97cbc3c508f1c1676dbedb52aa81edcdf7
GilianS <r0885351@student.thomasmore.be>
2022-05-11 13:38:14 +0200

    added combell setup files

---
+++ www/precache-manifest.27e98fdc299fd8ed1b28bb68590c6f3e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "7743e9fcc86d508c2985",
+    "url": "/static/css/main.6d4c3d08.chunk.css"
+  },
+  {
+    "revision": "7743e9fcc86d508c2985",
+    "url": "/static/js/main.4a7668b1.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "d6371772e7d09ae5b4f7",
+    "url": "/static/js/2.7986cba0.chunk.js"
+  },
+  {
+    "revision": "de01e9fd47a3a5c9fb668356962807b8",
+    "url": "/static/media/Woordmerk_A.de01e9fd.svg"
+  },
+  {
+    "revision": "943622a978449f61af1735d305a5130f",
+    "url": "/static/media/Logo_A.943622a9.svg"
+  },
+  {
+    "revision": "af6ea62714f4dc4aff98b14cc8ff23db",
+    "url": "/static/media/Philosopher-Regular.af6ea627.ttf"
+  },
+  {
+    "revision": "b51616787590e61c0198962dfd3872ca",
+    "url": "/static/media/Laila-Regular.b5161678.ttf"
+  },
+  {
+    "revision": "67c86e5dccfebcb1372419c9db0f1b0c",
+    "url": "/index.html"
+  }
+];
